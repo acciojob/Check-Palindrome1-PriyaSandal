5acee15df754cb17a4d5022b3d5a99569da3f200
PriyaSandal <133044444+PriyaSandal@users.noreply.github.com>
2023-06-15 13:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // complete the given function
+const string= str.toLowerCase();
 
 function palindrome(str){
  for(let i=0; i<str.length; i++){
-    if(str[i] !==str[length-1-i])
+    if(string[i] !==string[length-1-i])
 
 	  {
         return false;
